4a532accf0a4724d39d7851eaddf2b1630245c63
olegchursin <olegchursin@gmail.com>
2018-04-23 14:37:45 -0400

    attach CSS classes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,6 @@
 new Vue ({
   el: '#app',
   data: {
-    value: 0
-  },
-  computed: {
-    result() {
-      return this.value > 37 ? 'Done' : 'Not there yet';
-    }
-  },
-  watch: {
-    result: function(value) {
-      let bind = this;
-      setTimeout(function() {
-        bind.value = 0;
-      },5000);
-    }
+    attachRed: false
   }
 });
