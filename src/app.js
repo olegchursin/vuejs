704249438fd1824ccb708a8dcc03a8b313a02007
olegchursin <olegchursin@gmail.com>
2018-04-23 13:37:00 -0400

    2-way data binding with v-model

--- src/app.js
+++ src/app.js
@@ -1,18 +1,6 @@
 new Vue ({
-  el: '#exercise',
+  el: '#app',
   data: {
-    value: '',
-    valueEn: ''
-  },
-  methods: {
-    alertTrigger: function() {
-      alert("Hey! Hey!");
-    },
-    onKeydown: function(event) {
-      this.value = event.target.value;
-    },
-    onKeydownEnter: function(event) {
-      this.valueEn = event.target.value;
-    }
+    name: 'Oleg'
   }
 });
